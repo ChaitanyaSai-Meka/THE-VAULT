927064905cdaa49ba2e61f2ecc5a49e464caa8f6
Chaitanya Sai Meka <marvelchaitanya@gmail.com>
2025-11-24 16:01:17 +0530

    changed the port

--- the-vault/main.py
+++ the-vault/main.py
@@ -2,5 +2,5 @@ import uvicorn
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10002))
     uvicorn.run("app.app:app", host="0.0.0.0", port=port)
